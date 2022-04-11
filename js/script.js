3b81e71a2521b2762f73bfbf56f5c5e68f3b9ffb
stefanocravotta <you@example.com>
2022-04-11 13:54:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+/* 
+1. Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere (creare quindi una lista di utenti autorizzati),
+stampa un messaggio appropriato sull’esito del controllo.
+2. Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+
+//chiediamo all'utente la sua mail
+
+
